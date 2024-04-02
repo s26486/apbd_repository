dbf0ec3c7fe6aa9a52e6ef457732c8b1a9838cf1
s26486 <s26486@pjwstk.edu.pl>
2024-04-02 21:43:06 +0200

    average

--- Lab1/Labs/Program.cs
+++ Lab1/Labs/Program.cs
@@ -1,9 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Initial project");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Changing code v2");
-Console.WriteLine("Final changes v3");
+﻿
+ class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine("Initial project");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Changing code v2");
+        Console.WriteLine("Final changes v3");
 
-Console.Write("Modification v1");
-Console.Write("Modification v2");
-Console.Write("Modification v3");
+        Console.Write("Modification v1");
+        Console.Write("Modification v2");
+        Console.Write("Modification v3");
+
+    }
+    
+    
+
+    static double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Array is null or empty");
+        }
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
