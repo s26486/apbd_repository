2fdff78e7b93fda30247b039b9e854bad5300f8f
s26486 <s26486@pjwstk.edu.pl>
2024-04-02 21:53:57 +0200

    Modified average feature, but from the main

--- Lab1/Labs/Program.cs
+++ Lab1/Labs/Program.cs
@@ -31,9 +31,9 @@
         }
 
         int sum = 0;
-        foreach (int num in array)
+        foreach (int n in array)
         {
-            sum += num;
+            sum += n;
         }
 
         return (double)sum / array.Length;
