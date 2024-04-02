00b1f33ba9ea60a6dbcb3ce2e116369fa30f439e
s26486 <s26486@pjwstk.edu.pl>
2024-04-02 21:35:45 +0200

    initial project push

---
+++ Lab1/Labs/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Initial project");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Changing code v2");
+Console.WriteLine("Final changes v3");
